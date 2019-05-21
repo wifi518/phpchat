6b278d704ab3d9478205bcca62be9d1ea07d8e42
Alex Wifi <wifi518@1av.at>
2019-05-21 09:50:59 +0200

    index erstellt

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Chat</title>
+  </head>
+  <body>
+    <h1>PHP Chat</h1>
+    <?php
+    echo 'PHP works.';
+    ?>
+  </body>
+</html>
